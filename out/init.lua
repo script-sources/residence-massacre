c117c125234ddb1cb1f34ddc6ccb6462b698ac6f
TerminalVibes <87617218+TerminalVibes@users.noreply.github.com>
2024-03-10 15:07:55 -0400

    Updated loader

--- out/init.lua
+++ out/init.lua
@@ -1,3 +1,21 @@
 -- Compiled with roblox-ts v2.3.0
-loadstring(game:HttpGet(""))()
+local ROOT = "https://raw.githubusercontent.com/script-sources/residence-massacre/main/out/"
+local SOURCES = {
+	[1] = "night-1.lua",
+	[2] = "night-2.lua",
+}
+local _binding = game
+local GameId = _binding.GameId
+local PlaceId = _binding.PlaceId
+-- Check if the game is Residence Massacre
+if GameId ~= 14437001043 then
+	error("[Invalid Game]: Residence Massacre expected")
+end
+-- Load the source by PlaceId
+local path = SOURCES[PlaceId]
+if path ~= nil then
+	loadstring(game:HttpGet(ROOT .. path))()
+else
+	error("[Invalid Place]: Night 1 or Night 2 expected")
+end
 return nil
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-loadstring(game.HttpGet(""))();
+const ROOT = "https://raw.githubusercontent.com/script-sources/residence-massacre/main/out/";
+const SOURCES = new Map<number, string>([
+	[1, "night-1.lua"],
+	[2, "night-2.lua"],
+]);
+
+const { GameId, PlaceId } = game;
+
+// Check if the game is Residence Massacre
+if (GameId !== 14437001043) throw "[Invalid Game]: Residence Massacre expected";
+
+// Load the source by PlaceId
+const path = SOURCES.get(PlaceId);
+if (path !== undefined) loadstring(game.HttpGet(ROOT + path))();
+else throw "[Invalid Place]: Night 1 or Night 2 expected";
